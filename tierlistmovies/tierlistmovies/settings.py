9d2269c8ad0d648fcf0fb8d33f5dcaeb79e032db
Alvaro <devalvper@gmail.com>
2024-08-23 00:39:09 -0300

    finaly fix css deplyment

--- tierlistmovies/tierlistmovies/settings.py
+++ tierlistmovies/tierlistmovies/settings.py
@@ -122,15 +122,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tierlistmovies/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tierlistmovies/static')]
 
-STATICFILES_DIRS = ['../projects/6.project_api_movies/tierlistmovies/tierlistmovies/static']
-
-
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '../tierlistmovies/staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
