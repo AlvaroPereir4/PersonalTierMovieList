535b447b9c628a8118a730b69d88f0917d6fd073
Alvaro <devalvper@gmail.com>
2024-08-23 02:58:20 -0300

    refined build-files.sh to fix problens of version of python

--- tierlistmovies/build_files.sh
+++ tierlistmovies/build_files.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Instalar Python 3
+yum install -y python3
+
+# Atualizar pip para Python 3
+python3 -m pip install --upgrade pip
+
+# Instalar dependências do projeto
 pip install -r requirements.txt
-python --version
-python manage.py collectstatic --noinput
+
+# Verificar a versão do Python para garantir que é o 3.x
+python3 --version
+
+# Executar o comando de build
+npm run build
+
+# Executar o comando de coleta de arquivos estáticos do Django
+python3 manage.py collectstatic --noinput
