b82264b4004833b4cdda6f815e64b9841f028e4c
Alvaro <devalvper@gmail.com>
2024-08-23 02:47:40 -0300

    att to fix deploy

--- tierlistmovies/manage.py
+++ tierlistmovies/manage.py
@@ -13,7 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        )
+        ) from exc  # Explicitly re-raise with the original exception
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
