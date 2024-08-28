a3a8a3ebb32d6f6ca12dd886af8fa9f617164967
Alvaro <devalvper@gmail.com>
2024-08-28 01:16:23 -0300

    git ignore and sh update

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,10 @@ python3 --version
 yum install -y python3
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
+# Garantir que o diretório de arquivos estáticos seja limpo
+rm -rf tierlistmovies/staticfiles
+mkdir -p tierlistmovies/staticfiles
+
 python3 --version
 python3 tierlistmovies/manage.py collectstatic --noinput
