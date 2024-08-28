4f234618a8867c4356561b787078d372b74a90f0
Alvaro <devalvper@gmail.com>
2024-08-28 00:19:52 -0300

    files sh update

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ yum install -y python3
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 --version
-python3 manage.py collectstatic --noinput
+python3 tierlistmovies/manage.py collectstatic --noinput
