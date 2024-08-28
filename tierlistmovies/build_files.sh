38c531ea78d6025d7e308497288afc58736befca
Alvaro <devalvper@gmail.com>
2024-08-27 23:27:13 -0300

    sh files update

--- tierlistmovies/build_files.sh
+++ tierlistmovies/build_files.sh
@@ -1,25 +1,7 @@
-chmod +x build_files.sh
-yum install -y python3
-
-# Atualizar pip para a versão mais recente
-echo "Atualizando pip..."
-python3 -m pip install --upgrade pip
-
-# Instalar dependências do projeto
-echo "Instalando dependências do projeto..."
-pip install -r requirements.txt
-
-# Verificar a versão do Python para garantir que é a 3.x
-echo "Versão do Python:"
 python3 --version
-
-# Executar o comando de build (se houver algum build específico do npm)
-# Se o projeto não tiver um comando `npm run build`, você pode remover ou comentar essa linha.
-echo "Executando o comando de build do npm..."
+yum install -y python3
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+python3 --version
 npm run build
-
-# Executar o comando de coleta de arquivos estáticos do Django
-echo "Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput
-
-echo "Script de build concluído com sucesso."
