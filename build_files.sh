7974644394b4ad1413166ece6341c126a83b20b9
Alvaro <devalvper@gmail.com>
2024-09-01 14:42:49 -0300

    build_files.sh update for env better django

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,32 @@
+#!/bin/bash
+set -e
+
+# Verificar versão do Python
+echo "Verificando versão do Python:"
 python3 --version
+
+# Instalar Python 3 se necessário
 yum install -y python3
+
+# Atualizar pip
 python3 -m pip install --upgrade pip
+
+# Instalar dependências do projeto
 pip install -r requirements.txt
 
-# Garantir que o diretório de arquivos estáticos seja limpo
+# Garantir que o diretório de arquivos estáticos seja limpo e criado
+echo "Limpando e criando o diretório de arquivos estáticos:"
 rm -rf tierlistmovies/staticfiles
 mkdir -p tierlistmovies/staticfiles
 
+# Verificar a versão do Python novamente (opcional)
+echo "Verificando versão do Python novamente:"
 python3 --version
+
+# Coletar arquivos estáticos
+echo "Coletando arquivos estáticos:"
 python3 tierlistmovies/manage.py collectstatic --noinput
 
-# Verifica o conteúdo do diretório staticfiles
-echo "Conteúdo do diretório tierlistmovies:"
+# Verificar o conteúdo do diretório staticfiles
+echo "Conteúdo do diretório tierlistmovies/staticfiles:"
 ls -R tierlistmovies/staticfiles
