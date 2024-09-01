3d127ea255af9a61cf355c4b6138eb0c40f4ce63
Alvaro <devalvper@gmail.com>
2024-09-01 14:33:08 -0300

    asgi update

--- tierlistmovies/tierlistmovies/asgi.py
+++ tierlistmovies/tierlistmovies/asgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tierlistmovies.settings')
-
 application = get_asgi_application()
 app = application
