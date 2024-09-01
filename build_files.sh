fd2f7bfa24a5e6a5043483683c0cc1c554dc00bd
Alvaro <devalvper@gmail.com>
2024-09-01 15:01:40 -0300

    fix deploy

--- build_files.sh
+++ build_files.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 set -e
 
-# Verificar versão do Python
-echo "Verificando versão do Python:"
-python3 --version
-
-# Instalar Python 3 se necessário
-yum install -y python3
-
 # Criar e ativar ambiente virtual
 python3 -m venv venv
 source venv/bin/activate
 
-# Atualizar pip
+# Atualizar pip e instalar dependências
 pip install --upgrade pip
-
-# Instalar dependências do projeto
 pip install -r requirements.txt
 
-# Garantir que o diretório de arquivos estáticos seja limpo e criado
-echo "Limpando e criando o diretório de arquivos estáticos:"
-rm -rf tierlistmovies/staticfiles
-mkdir -p tierlistmovies/staticfiles
-
-# Verificar a versão do Python novamente (opcional)
-echo "Verificando versão do Python novamente:"
-python3 --version
-
 # Coletar arquivos estáticos
-echo "Coletando arquivos estáticos:"
 python3 tierlistmovies/manage.py collectstatic --noinput
 
-# Verificar o conteúdo do diretório staticfiles
-echo "Conteúdo do diretório tierlistmovies/staticfiles:"
-ls -R tierlistmovies/staticfiles
+# Aplicar migrações do banco de dados
+python3 tierlistmovies/manage.py migrate
