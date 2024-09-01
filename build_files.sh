195b7ce4e1537fbbd1a661b7a2cf8cbff1524a48
Alvaro <devalvper@gmail.com>
2024-09-01 12:06:38 -0300

    vercel update to fix deploy

--- build_files.sh
+++ build_files.sh
@@ -11,5 +11,5 @@ python3 --version
 python3 tierlistmovies/manage.py collectstatic --noinput
 
 # Verifica o conteúdo do diretório staticfiles
-echo "Conteúdo do diretório staticfiles:"
-ls -R /vercel/path0/tierlistmovies/staticfiles/
+echo "Conteúdo do diretório tierlistmovies:"
+ls -R /vercel/path0/tierlistmovies/
